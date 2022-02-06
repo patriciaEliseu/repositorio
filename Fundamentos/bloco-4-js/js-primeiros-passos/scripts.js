fd80674f91091abc4c3d713dc0a97cebc327e4fe
Patricia Eliseu <patyeliseu4@gmail.com>
2022-02-06 20:23:50 -0300

    ad exercicio primeiros passos

---
+++ Fundamentos/bloco-4-js/js-primeiros-passos/scripts.js
@@ -0,0 +1,9 @@
+const myName = 'Patricia';
+const birthCity = 'Rio de Janeiro';
+let birthYear = 1972;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(myName, birthCity, birthYear);
