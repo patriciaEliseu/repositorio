9cdcc2ae84e9e599f64c9f981882c38d0a7b6d1f
Patricia Eliseu <patyeliseu4@gmail.com>
2022-03-08 11:48:11 -0300

    teste

---
+++ Fundamentos/bloco-7-ES6/dia-03/sum.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('parameters must be numbers'); /* os parametros devem ser numeros */
+    }
+
+    return a + b;
+}
