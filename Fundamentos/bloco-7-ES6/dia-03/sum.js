df1d4d1f4f2a98f60421542965fb4a7eec25b25f
Patrícia Gonçalves Eliseu <97802219+patriciaEliseu@users.noreply.github.com>
2022-03-07 21:44:46 -0300

    Delete Fundamentos/bloco-7-ES6/dia-03 directory

--- Fundamentos/bloco-7-ES6/dia-03/sum.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        throw new Error('parameters must be numbers'); /* os parametros devem ser numeros */
-    }
-
-    return a + b;
-}
-
-module.exports = sum;
